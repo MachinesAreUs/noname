b120001c618c62c0750539ff79f4235490e106e5
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-07-08 12:58:16 -0500

    Added Image model

---
+++ priv/repo/migrations/20150708174932_create_image.exs
@@ -0,0 +1,18 @@
+defmodule Iris.Repo.Migrations.CreateImage do
+  use Ecto.Migration
+
+  def change do
+    create table(:images) do
+      add :headline, :string
+      add :sub_headline, :string
+      add :country, :string
+      add :provider, :string
+      add :copyright_line, :string
+      add :creation_date, :date
+      add :embargo, :date
+
+      timestamps
+    end
+
+  end
+end
