893e66957412e6824a53ac6cd07c4908d545d550
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-07-17 16:14:05 -0500

    Fixed deprecation warnings

--- test/controllers/subject_code_controller_test.exs
+++ test/controllers/subject_code_controller_test.exs
@@ -16,7 +16,7 @@ defmodule Iris.SubjectCodeControllerTest do
   end
 
   test "shows chosen resource", %{conn: conn} do
-    subject_code = Repo.insert %SubjectCode{}
+    subject_code = Repo.insert! %SubjectCode{}
     conn = get conn, subject_code_path(conn, :show, subject_code)
     assert json_response(conn, 200)["data"] == %{
       "id" => subject_code.id
@@ -35,20 +35,20 @@ defmodule Iris.SubjectCodeControllerTest do
   end
 
   test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    subject_code = Repo.insert %SubjectCode{}
+    subject_code = Repo.insert! %SubjectCode{}
     conn = put conn, subject_code_path(conn, :update, subject_code), subject_code: @valid_attrs
     assert json_response(conn, 200)["data"]["id"]
     assert Repo.get_by(SubjectCode, @valid_attrs)
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    subject_code = Repo.insert %SubjectCode{}
+    subject_code = Repo.insert! %SubjectCode{}
     conn = put conn, subject_code_path(conn, :update, subject_code), subject_code: @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "deletes chosen resource", %{conn: conn} do
-    subject_code = Repo.insert %SubjectCode{}
+    subject_code = Repo.insert! %SubjectCode{}
     conn = delete conn, subject_code_path(conn, :delete, subject_code)
     assert json_response(conn, 200)["data"]["id"]
     refute Repo.get(SubjectCode, subject_code.id)
--- web/controllers/subject_code_controller.ex
+++ web/controllers/subject_code_controller.ex
@@ -15,7 +15,7 @@ defmodule Iris.SubjectCodeController do
     changeset = SubjectCode.changeset(%SubjectCode{}, subject_code_params)
 
     if changeset.valid? do
-      subject_code = Repo.insert(changeset)
+      subject_code = Repo.insert!(changeset)
       render(conn, "show.json", subject_code: subject_code)
     else
       conn
@@ -34,7 +34,7 @@ defmodule Iris.SubjectCodeController do
     changeset = SubjectCode.changeset(subject_code, subject_code_params)
 
     if changeset.valid? do
-      subject_code = Repo.update(changeset)
+      subject_code = Repo.update!(changeset)
       render(conn, "show.json", subject_code: subject_code)
     else
       conn
@@ -46,7 +46,7 @@ defmodule Iris.SubjectCodeController do
   def delete(conn, %{"id" => id}) do
     subject_code = Repo.get(SubjectCode, id)
 
-    subject_code = Repo.delete(subject_code)
+    subject_code = Repo.delete!(subject_code)
     render(conn, "show.json", subject_code: subject_code)
   end
 end
